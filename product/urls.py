53ab21e20e2a891e28b6804f651471af318ce044
Hasib Alamyar <hasibalamyar2@gmail.com>
2025-08-11 04:17:43 +0430

    feat(Product): detail_view(views.py) & get_url(Models.py) added in product app.

--- product/urls.py
+++ product/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('product_list/' , views.product_list , name = 'product_list'),
-    path('category/<slug:category_slug>' , views.products_by_category , name = 'products_by_category'),
-    path('category/<slug:category_slug>/<slug:product_slug>' , views.product_detail , name = 'product_detail'),
+    path('<slug:category_slug>' , views.products_by_category , name = 'products_by_category'),
+    path('category/<slug:category_slug>/<slug:product_slug>/' , views.product_detail , name = 'product_detail'),
+    # path('<slug:category_slug>/<slug:product_slug>/' , views.product_detail , name='product_detail'),
+
 ]
