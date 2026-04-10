abf68bdc12f8e89cec566c0c23034629b874a02c
HasibAFG <hasibalamyar2@gmail.com>
2026-04-11 01:08:14 +0430

    Category duplicate record solved!

--- category/admin.py
+++ category/admin.py
@@ -3,9 +3,22 @@ from .models import Category
 from parler.admin import TranslatableAdmin
 
 
+# @admin.register(Category)
+# class CategoryAdmin(TranslatableAdmin):
+#     list_display = ('category_name', 'slug', 'description')
+#     search_fields = ('translations__category_name',)
+#     ordering = ('translations__category_name',)
+#     list_per_page = 20
+
 @admin.register(Category)
 class CategoryAdmin(TranslatableAdmin):
     list_display = ('category_name', 'slug', 'description')
     search_fields = ('translations__category_name',)
     ordering = ('translations__category_name',)
     list_per_page = 20
+    ordering = ('slug',)
+    list_per_page = 20
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.distinct()
